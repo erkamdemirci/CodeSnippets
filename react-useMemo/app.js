628c0ff18c5cea112e6fd1abc522b37bc2eaff67
Erkam Demirci <erkamdmrc@gmail.com>
2022-03-16 17:02:40 +0300

    master

--- react-useMemo/app.js
+++ react-useMemo/app.js
@@ -1,27 +1,27 @@
-// Internal React Engine useCallback Example //
+// Internal React Engine useMemo Example //
 
 let memoized = []
 let numOfCalls = 0
-let callback
+let returnedValue
 
-const useCallback = (fn, arr) => {
+const useMemo = (fn, arr) => {
   numOfCalls++
 
   let returnedFunc = {}
 
   if (numOfCalls === 1) {
-    callback = fn
+    returnedValue = fn()
     memoized = [...arr]
-    returnedFunc.fn = callback
+    returnedFunc.fn = returnedValue
   }
 
   if (numOfCalls > 1) {
     memoized.forEach((el) => {
       arr.forEach((elm) => {
         if (el === elm) {
-          returnedFunc.fn = callback
+          returnedFunc.fn = returnedValue
         } else {
-          returnedFunc.fn = fn
+          returnedFunc.fn = fn()
         }
       })
     })
@@ -33,11 +33,11 @@ const useCallback = (fn, arr) => {
 // React Code //
 let running = 0
 const App = (dependency) => {
-  const func = useCallback(() => {
+  const func = useMemo(() => {
     running++
     console.log('I run', running, running === 1 ? 'time' : 'times', running === 1 ? '- first time' : '- second time')
+    return dependency
   }, [dependency])
-
   return func
 }
 
@@ -49,6 +49,4 @@ const res1 = App(dependency1)
 //* ==== second render ==== //
 const res2 = App(dependency2)
 
-res1.fn()
-res2.fn()
-console.log('has the function returned by useCallback the same identity?:', res1.fn === res2.fn)
+console.log('is the value returned by useMemo the same?:', res1.fn === res2.fn, res1.fn, res2.fn)
